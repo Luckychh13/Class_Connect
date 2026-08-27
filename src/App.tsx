7a92031dd8abf579f762d82b971665fe3ecebcfd
Luckychh13 <luckychh005@gmail.com>
2026-08-28 00:10:58 +0530

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -25,11 +25,14 @@ import { useNotificationProvider } from "./components/refine-ui/notification/use
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import "./App.css";
+import Dashboard from "./pages/dashboard";
+import { BookOpen, Home } from "lucide-react";
+import SubjectsList from "./pages/subjects/List";
+import SubjectsCreate from "./pages/subjects/Create";
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <ThemeProvider>
           <DevtoolsProvider>
@@ -42,9 +45,32 @@ function App() {
                 warnWhenUnsavedChanges: true,
                 projectId: "Pi909j-Ik8l8H-n8dVAg",
               }}
+              resources={[
+                {
+                  name:'dashboard',
+                  list:'/',
+                  meta:{label:'Home',icon:<Home />}
+                },
+                {
+                  name:'subjects',
+                  list:'/subjects',
+                  create:'/subjects/create',
+                  meta:{label:'Subjects',icon:<BookOpen />}
+                }
+              ]}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
+                <Route element={
+                  <Layout>
+                    <Outlet />
+                  </Layout>
+                }>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="subjects">
+                  <Route index element={<SubjectsList />} />
+                  <Route path="create" element={<SubjectsCreate />} />
+                </Route>
+                </Route>
               </Routes>
               <Toaster />
               <RefineKbar />
