fdfcc73e761b1a1bd34566d189349a9a4fe25b9d
Luckychh13 <luckychh005@gmail.com>
2026-09-02 22:04:57 +0530

    implement classes page with form

--- src/App.tsx
+++ src/App.tsx
@@ -15,20 +15,17 @@ import routerProvider, {
   DocumentTitleHandler,
 } from "@refinedev/react-router";
 import { dataProvider } from "./providers/data";
-import { Login } from "./pages/login";
-import { Register } from "./pages/register";
-import { ForgotPassword } from "./pages/forgot-password";
-import { ErrorComponent } from "./components/refine-ui/layout/error-component";
 import { Layout } from "./components/refine-ui/layout/layout";
-import { Header } from "./components/refine-ui/layout/header";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import "./App.css";
-import Dashboard from "./pages/dashboard";
-import { BookOpen, Home } from "lucide-react";
+import Dashboard from "./pages/Dashboard";
+import { BookOpen, GraduationCap, Home } from "lucide-react";
 import SubjectsList from "./pages/subjects/List";
 import SubjectsCreate from "./pages/subjects/Create";
+import ClassesList from "./pages/classes/list";
+import ClassesCreate from "./pages/classes/create";
 
 function App() {
   return (
@@ -56,6 +53,12 @@ function App() {
                   list:'/subjects',
                   create:'/subjects/create',
                   meta:{label:'Subjects',icon:<BookOpen />}
+                },
+                {
+                  name:'classes',
+                  list:'/classes',
+                  create:'/classes/create',
+                  meta:{label:'Classes',icon:<GraduationCap />}
                 }
               ]}
             >
@@ -66,10 +69,17 @@ function App() {
                   </Layout>
                 }>
                 <Route path="/" element={<Dashboard />} />
+
                 <Route path="subjects">
                   <Route index element={<SubjectsList />} />
                   <Route path="create" element={<SubjectsCreate />} />
                 </Route>
+
+                <Route path="classes">
+                  <Route index element= {<ClassesList />} />
+                  <Route path="create" element= {<ClassesCreate />} />
+                </Route>
+
                 </Route>
               </Routes>
               <Toaster />
---
+++ src/pages/classes/list.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const list = () => {
+  return (
+    <div>
+      LIST
+    </div>
+  )
+}
+
+export default list
