ab2b2c77f24e3e5aa8d0b66687a16fae4d45d3c5
Luckychh13 <luckychh005@gmail.com>
2026-09-03 13:07:15 +0530

    implement cloudinary widget image

--- src/pages/classes/create.tsx
+++ src/pages/classes/create.tsx
@@ -19,17 +19,17 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import {Label} from "@/components/ui/label.tsx";
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select.tsx";
 import {Textarea} from "@/components/ui/textarea.tsx";
 import {Loader2} from "lucide-react";
 import { subjects, teachers } from "@/constants";
+import UploadWidget from "@/constants/upload-widget";
 
 
 const Create = () => {
     const back = useBack();
 
-    const form = useForm({
+    const form = useForm<z.infer<typeof classSchema>>({
         resolver: zodResolver(classSchema),
         refineCoreProps: {
             resource: "classes",
@@ -42,7 +42,7 @@ const Create = () => {
 
     const {
         handleSubmit,
-        formState: { isSubmitting },
+        formState: { isSubmitting, errors },
         control,
     } = form;
 
@@ -54,6 +54,22 @@ const Create = () => {
         }
     };
 
+    const bannerPublicId = form.watch('bannerCldPubId')
+    const setBannerImage = (file,field) => {
+        if(file){
+            field.onChange(file.url)
+            form.setValue('bannerCldPubId',file.piblicId,{
+                shouldDirty:true,
+                shouldValidate:true
+            })
+        }else{
+            field.onChange('')
+            form.setValue('bannerCldPubId', '', {
+                shouldDirty:true,
+                shouldValidate:true
+            })
+        }
+    }
     return (
         <CreateView className="class-view">
             <Breadcrumb />
@@ -79,13 +95,25 @@ const Create = () => {
                     <CardContent className="mt-7">
                         <Form {...form}>
                             <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
-                                <div className="space-y-3">
-                                    <Label>
-                                        Banner Image <span className="text-orange-600">*</span>
-                                    </Label>
-
-                                    <p>Upload image widget</p>
-                                </div>
+                                <FormField
+                                   control={control}
+                                   name="bannerUrl"
+                                   render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Banner Image <span className="text-orange-600">*</span></FormLabel>
+                                        <FormControl>
+                                            <UploadWidget
+                                              value={field.value ? {url:field.value, publicId:bannerPublicId ?? ''}: null}
+                                              onchange={(file:any,field:any) => setBannerImage(file,field)}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                        {errors.bannerCldPubId && !errors.bannerUrl && (
+                                            <p className="text-destructive text-sm">{errors.bannerCldPubId.message?.toString()}</p>
+                                        )}
+                                    </FormItem>
+                                   )}
+                                />
 
                                 <FormField
                                     control={control}
